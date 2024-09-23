679a4fc33b97e1d0e7b2eb055810e52c0f5ea3d8
Mark Edmunds <36555602+rhinoed@users.noreply.github.com>
2024-09-23 14:15:23 -0700

    Remove hard coded values for models
    Removed the two hard coded models which could be used
    Added check_for_model function to check if provided model exist
    Can now display list of available models

--- Scripts/Shell/generate_ERD.sh
+++ Scripts/Shell/generate_ERD.sh
@@ -5,9 +5,23 @@
 # 9/11/2024
 
 # Check which model to use
-echo "Which model do you want to use?"
-echo "Enter 1 to use llama 3.1 (local)"
-echo "Enter 2 to use gemini-1.5-pro-latest (remote)"
+echo "Do you want to run the model locally? Y/N"
+read local
+if [ "$local" == "Y" ] || [ "$local" == "y" ] && command -v ollama >/dev/null 2>&1
+    then
+        ollama ps
+    elif [ "$local" == "Y" ] || [ "$local" == "y" ]
+    then 
+        echo "Ollama is not installed"
+fi
+echo "Do you want to see a list of available models? Y/N"
+read answer
+if [ "$answer" == "Y" ] || [ "$answer" == "y" ] && command -v fabric >/dev/null 2>&1 # check if fabric is installed if not the expression will evaluate to 0
+    then
+        echo "$( fabric -L )" | less
+fi
+echo "Which model do you want to use? Leave blank to use default"
+
 # Read user Input
 read model
 file=$1
@@ -16,31 +30,53 @@ file=$1
 generate_ERD(){
     local file=$1
     local model=$2
+    local ERD_name=$3
     
     # The previous checks for ollama and fabric did not work found this solution using ChatGPT
-    if [ "$model" == "1" ] && command -v ollama >/dev/null 2>&1 # check if ollama is installed if not the expression will evaluate to 0
+    if [ "$model" != "" ] && command -v fabric >/dev/null 2>&1 # check if fabric is installed if not the expression will evaluate to 0
         then
-            # start ollama
-            ollama serve
-            # generate ERD using llama 3.1
-            cat "$file" | fabric --model llama3.1:latest --output package_ERD.md -p create_erd
-        elif [ "$model" == "2" ] && command -v fabric >/dev/null 2>&1 # check if fabric is installed if not the expression will evaluate to 0
+            # generate ERD using the model provided
+            cat "$file" | fabric --model "$model" --output "$ERD_name" -p create_erd_ai
+        elif [ "$model" == "" ] && command -v fabric >/dev/null 2>&1 # check if fabric is installed if not the expression will evaluate to 0
         then
-            # generate ERD using gemini-1.5-pro-latest
-            cat "$file" | fabric --model models/gemini-1.5-pro-latest --output package_ERD.md -p create_erd
+            # generate ERD using default model
+            cat "$file" | fabric --output "$ERD_name" -p create_erd_ai
         else
             # exit with error
-            echo "Invalid model selected or framework not installed"
+            echo "fabric framework not installed"
             # exit with error code 1
             exit 1
+    fi
+}
+
+# Check if model exist
+check_for_model (){
+    local model="$1"
+    result=$(fabric -L | grep "$model")
+    if [ "$result" != "" ]
+        then
+            return 0
+        else
+            return 1
     fi
 }
 
 # Check if file exist
 if [ -f "$file" ]
     then
-        # call the function to generate ERD
-        generate_ERD "$file" "$model"
+        # check if model exist
+        if check_for_model "$model"
+            then
+                # call the function to generate ERD
+                echo "what name do you want to give to the ERD file?"
+                read ERD_name
+                echo "Generating ERD..."
+                generate_ERD "$file" "$model" "$ERD_name"   
+            else
+                echo "Model does not exist"
+                exit 1
+        fi
+        
     else
         # provide reason for exit
         echo "No argument provided expected file path or file name"
